a6debd9788bfc6473dfe77424f2a04ca8be1ead0
kojiyamauchi <all.the.small.things@me.com>
2021-02-01 20:24:33 +0900

    some update.

--- apps/bs-config.js
+++ apps/bs-config.js
@@ -5,5 +5,6 @@ module.exports = {
   files: ['./resource/**/*', `${root}/**/*`],
   proxy: 'localhost:3000',
   port: 4000,
-  browser: 'google chrome canary'
+  browser: 'google chrome canary',
+  reloadDelay: 3500
 }
