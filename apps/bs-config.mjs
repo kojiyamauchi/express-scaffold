aaeb258193e02c176ef396073d0555b93d1b7580
Koji <all.the.small.things@me.com>
2025-01-27 19:51:26 +0900

    replace cjs -> mjs bs-config.

--- apps/bs-config.mjs
+++ apps/bs-config.mjs
@@ -1,17 +1,20 @@
-const path = require('path')
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const root = path.resolve(`${__dirname}/` + '../delivery/')
 
-module.exports = {
+export default {
   files: ['./resource/**/*', `${root}/**/*`],
   https: true,
   open: false,
   proxy: {
-    target: 'https://localhost:3000'
+    target: 'https://localhost:3000',
   },
   port: 8000,
   browser: 'google chrome canary',
   reloadDelay: 10000, // TODO: PC買い替えて確認する
   ui: {
-    port: 9999
-  }
+    port: 9999,
+  },
 }
