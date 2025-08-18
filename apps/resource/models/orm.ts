11b0cd4c6f95e0dd5471e80285cfb0152f640174
Koji <all.the.small.things@me.com>
2025-08-18 19:37:20 +0900

    add orm models.

---
+++ apps/resource/models/orm.ts
@@ -0,0 +1,35 @@
+import { Item, Order, OrderItem, User } from '@prisma/client'
+
+import { prisma } from '@/libs'
+
+export const ormModels = {
+  users: async (): Promise<User[]> => {
+    return prisma.user.findMany({
+      include: {
+        order: true,
+      },
+    })
+  },
+  items: async (): Promise<Item[]> => {
+    return prisma.item.findMany({
+      include: {
+        order_items: true,
+      },
+    })
+  },
+  orders: async (): Promise<Order[]> => {
+    return prisma.order.findMany({
+      include: {
+        order_items: true,
+      },
+    })
+  },
+  orderItems: async (): Promise<OrderItem[]> => {
+    return prisma.orderItem.findMany({
+      include: {
+        item: true,
+        order: true,
+      },
+    })
+  },
+}
